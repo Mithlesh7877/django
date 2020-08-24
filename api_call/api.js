6777c66b04f0949d6ad85ccc2d4c904359145dfe
Mithilesh Yadav <54845733+Mithlesh7877@users.noreply.github.com>
2020-08-24 18:59:01 +0530

    Update api.js

--- api_call/api.js
+++ api_call/api.js
@@ -1,19 +1,28 @@
-document.getElementById('getText').addEventListener('click',getText);
-function getText(){
+document.getElementById('Text').addEventListener('click',add_element_to_array);
+
+var x =5;
+var array = Array();
+(function getText(){
     fetch('https://randomuser.me/api/?results=10')
     .then(res=>{
       return  res.json();
     })
     .then(x=>{
-        console.log(x.results.slice(1,6));
-        const html= x.results.slice(1,6).map(user=>{
-                        return `
-            <h4>Name: ${user.name.first}</h4>
-            <p><img src ='${user.picture.large}' alt='${user.name.firtst}'></p> `
-        }).join(' ');
-        // console.log(html)
-        document.getElementById('output').innerHTML=html;
+        console.log(x.results);
+        array=x.results;
+        // console.log(array);
+        // add_element_to_array(array)
+    //     const html= x.results.map(user=>{
+    //                     return `
+    //         <h4>Name: ${user.name.first}</h4>
+    //         <p><img src ='${user.picture.large}' alt='${user.name.firtst}'></p> `
+    //     }).join(' ');
+    //     console.log(html)
+    //     document.getElementById('output').innerHTML=html;
     });
+
+
+    
         // document.getElementById('output').innerHTML=;    
     // fetch('sample.txt')
     //     .then(function(res){
@@ -28,4 +37,17 @@ function getText(){
     // .then((data)=>{
     //     document.getElementById('output').innerHTML=data;
     // })
+}());
+
+// const html=Array();
+function add_element_to_array(array){
+    console.log(array);
+    // for (var i =0;i<x;i++){
+    //    const html= `<h4>Name: ${array[i].name.first}</h4>
+    //    <p><img src ='${array[i].picture.large}' alt='${array[i].name.firtst}'></p>`
+    //             console.log(html);
+    //             document.getElementById('output').innerHTML+=html;           
+    // }
+    i=x;
+    x=x+5;
 }
